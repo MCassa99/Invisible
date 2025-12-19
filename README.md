2e26569b19402da403c6a80a58015e43c539ed2a
Matias Cassanello <mcassa1999@gmail.com>
2025-12-18 22:20:07 -0300

    Config

--- README.md
+++
@@ -1,3 +0,0 @@
-# Invisible
-# Invisible
-# Invisible
--- backend/contacts.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = [
-  { id: 1, name: "Belen", email: "belub19@icloud.com" },
-  { id: 2, name: "Daniel", email: "danbocks@gmail.com" },
-  { id: 3, name: "Mateo", email: "mateobocksberger@gmail.com" },
-  { id: 4, name: "Myriam", email: "migrett@gmail.com" },
-  { id: 5, name: "Julieta", email: "julietarabazza22@gmail.com" },
-  { id: 6, name: "Matias", email: "mcassa1999@gmail.com" }
-];
